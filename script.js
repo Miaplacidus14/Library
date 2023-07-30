26b078718a2c63bffb97816e1e51b1db7a3d8da6
Miaplacidus14 <colombangh@gmail.com>
2023-07-30 21:18:55 +0200

    Fin sans optimisation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function addBookToLibrary () {
     form.style.zIndex = "1000"
     const div1 = document.createElement('div');
     div1.classList.add("total");
-    form.appendChild(div1)
+    form.appendChild(div1);
     for (let i = 0; i < 4; i++) {
         const div = document.createElement('div');
         div.classList.add('form');
@@ -53,40 +53,73 @@ function addBookToLibrary () {
     button.setAttribute('type', 'submit');
     button.textContent = "Valider";
     div1.appendChild(button);
-    console.log(myLibrary);
     form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        const title = e.target.title.value;
-        const author = e.target.author.value;
-        const pages = e.target.pages.value;
-        const read = e.target.read.checked;
-        myLibrary.unshift(new Book(title, author, pages, read));
-        console.log(myLibrary);
         div1.remove();
-        addToPage();
-        form.style.zIndex = "-1000";
     });
 }
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const author = e.target.author.value;
+    const pages = e.target.pages.value;
+    const read = e.target.read.checked;
+    myLibrary.unshift(new Book(title, author, pages, read));
+    form.style.zIndex = "-1000";
+    addToPage();
+});
 
 function addToPage () {
     const library = document.querySelector('.library');
     const card = document.createElement('div');
     card.classList.add("card");
     library.appendChild(card);
-    
     for (const key in myLibrary[0]) {
         if (key === 'title') {
             const title = document.createElement('h1');
-            title.textContent = `${myLibrary[0].title}`
-            card.appendChild(title)
+            title.textContent = `${myLibrary[0].title}`;
+            card.setAttribute('data-name', `${myLibrary[0].title}`)
+            card.appendChild(title);
+        } else if (key === 'read') {
+            const button = document.createElement('button');
+            if (myLibrary[0].read === true) {
+                button.textContent = 'Read';
+                button.style.backgroundColor = 'green';
+            } else {
+                button.textContent = 'Not Read';
+                button.style.backgroundColor = 'red';
+            }
+            button.setAttribute('id', `${myLibrary[0].title}`)
+            button.setAttribute('onclick', "changeColor(this.id)")
+            card.appendChild(button)
         } else {
             const paragraph = document.createElement('p');
             paragraph.textContent = `${myLibrary[0][`${key}`]}`;
             card.appendChild(paragraph);
         }
     }
+    const secondButton = document.createElement('button');
+    secondButton.textContent = 'Remove';
+    secondButton.setAttribute('data-name', `${myLibrary[0].title}`)
+    secondButton.onclick = function() {
+        removeCard(this.getAttribute('data-name'));
+    };
+    card.appendChild(secondButton)
 }
 
+function changeColor(id) {
+    const button = document.getElementById(`${id}`);
+    if (button.style.backgroundColor === 'green') {
+        button.style.backgroundColor = 'red';
+        button.textContent = 'Not Read';
+    } else {
+        button.style.backgroundColor = 'green';
+        button.textContent = "Read"
+    }
+}
 
-
-
+function removeCard (data) {
+    const card = document.querySelector(`[data-name="${data}"]`);
+    card.remove();
+}
